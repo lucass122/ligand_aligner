a35a0a7a14c96a48eb9fc136626dddf99d0453a8
lucass122 <timo-niklas.lucas@student.uni-tuebingen.de>
2018-09-05 16:01:00 +0200

    fixed align script + automatic halogen bond finding works now

--- align.py
+++ align.py
@@ -1,7 +1,14 @@
-import pymol
+from pymol import cmd
 import numpy as np
+import pymol
 
 
+
+
+#INPUT PDB NAME HERE
+pdb = "4agl"
+#INPUT WHICH HALOGEN BOND TO USE FOR ALIGNMENT IF THERE ARE MULTIPLE
+hal_bond = 0
 
 
 """
@@ -142,7 +149,7 @@ def zero_z(selection,ref_atom):
 	return ang
 
 """
-Fucntion that aligns the selection to the negative x-axis of the origin.
+Function that aligns the selection to the negative x-axis of the origin.
 """
 
 def align_x_neg(selection,ref_atom):
@@ -155,46 +162,57 @@ def align_x_neg(selection,ref_atom):
 	angles.append(angle3)
 	return angles
 
+"""
+Function to get position of polar atoms that form halogen bonds with ligand
+"""
 def find_halogen_bonds():
-	cmd.select("halogen", "e. Cl or e. Br or e. I")	# select all relevant halogen atoms
-	number_of_halogens = cmd.select("halogen", "halogen and ligand")
-	if number_of_halogens == 0:
-		print("No halogens found in this file.")
-		return
+	halogen_bonds = []
+	cmd.select("bindingsite1", "ligand_a expand 5.0")	# expand selection by 5 angstrom, extend = extend along bonds
+	cmd.select("bindingsite1", "br. bindingsite")	# br extends the selection to the full residue
+	cmd.select("bindingsite1", "bindingsite and not ligand")	# br extends the selection to the full residue
+	cmd.select("water", "resn hoh")	# resn = residues name, hoh = name of water molecules
+	cmd.select("water_bs", "water and bindingsite")
 
+	#select all relevant halogen atoms
+	number_of_halogens = cmd.select("hal", "hal and ligand_a")
 	cmd.select("polar_atoms", "e. O or e. N or e. S")
-	model_halogen = cmd.get_model("halogen")	# create model for halogen selection
+
+	model_halogen = cmd.get_model("hal")	# create model for halogen selection
 	for halo in model_halogen.atom:
-		cmd.select("current_halogen", "id %s"%(halo.id))
-		#cmd.select("current_neighbor", "current_halogen extend 1")
-		#cmd.select("current_neighbor", "current_neighbor and not current_halogen")
+		print("A")
+		cmd.select("current_halogen", "(e. I or e. Cl or e. Br) and id %s"%(halo.id))
+		cmd.select("current_neighbor", "current_halogen extend 1")
+		cmd.select("current_neighbor", "current_neighbor and not current_halogen")
 		cmd.select("current_neighbor", "neighbor current_halogen")
-		cmd.select("bindingsite", "halligand expand 8.0")	# expand selection by 5 angstrom, extend = extend along bonds
-		cmd.select("bindingsite", "br. bindingsite")	# br extends the selection to the full residue
-		cmd.select("bindingsite", "bindingsite and not ligand")	# br extends the selection to the full residue
-		cmd.select("water", "resn hoh")	# resn = residues name, hoh = name of water molecules
-		cmd.select("water_bs", "water and bindingsite")
 
 		cmd.select("current_surroundings", "current_halogen expand 5.0")
-		cmd.select("current_surroundings", "current_surroundings and bindingsite")
+		cmd.select("current_surroundings", "current_surroundings and bindingsite1")
 		cmd.select("current_polar", "current_surroundings and polar_atoms")
+
 		model_polar = cmd.get_model("current_polar")
 		for pol in model_polar.atom:
-			cmd.select("current_polar", "id %s"%(pol.id))
+			cmd.select("current_polar", "polar_atoms and id %s"%(pol.id))
 			# check for halogen bonds, distances 5.0, angles: > 140
 			current_distance = cmd.distance("current_distance", "current_halogen", "current_polar")
 			current_angle = cmd.angle("current_angle", "current_neighbor", "current_halogen", "current_polar")
-		
 			if (current_angle >= 140) and (current_distance <= 5.0):
-				print("Halogen bond: ")
-				print current_distance, current_angle, pol.name, pol.resn, pol.resi
-				cmd.select(str(pol.resn)+"_"+str(pol.resi),"resn "+str(pol.resn)+" and resi " + str(pol.resi))
+				print current_distance, current_angle, pol.name, pol.resn, pol.resi, pol.id
+				halogen_bonds.append((pol.resn,pol.resi,pol.id))
+
 
 			cmd.delete("current_angle")
 			cmd.delete("current_distance")
+			
+			
 
-
-	
+	cmd.delete("current_halogen")
+	cmd.delete("current_neighbor")
+	cmd.delete("water")
+	cmd.delete("water_bs")
+	cmd.delete("current_polar")
+	cmd.delete("current_surroundings")
+	cmd.delete("bindingsite1")
+	return halogen_bonds
 
 def align_ligand(selection,center,angles):
 	print(center)
@@ -209,75 +227,74 @@ def align_ligand(selection,center,angles):
 
 	cmd.translate([translation_vector[0][0],translation_vector[0][1],translation_vector[0][2]],selection,-1,0)
 
-
-
 cmd.reinitialize()
 #LOAD FILES AND MAKE THE NECESSARY SELECTIONS FOR ALIGNMENT
-#TODO: REPLACE ITERATE STATE STUFF WITH FUNCTION CALL
-cmd.load("session_iodine_minus1_merged.pse", partial=1)
+cmd.load("session_chlorine_minus1_merged.pse", partial=1)
 cmd.do("run axes.py")
 #cmd.load("geometry_complex_Cl.mol")
 cmd.select("circ_complex","organic")
 
-cmd.fetch("1gjd")
-cmd.select("ligand","organic and 1gjd")
+cmd.fetch(pdb)
+#select ligand and bindingsite
+cmd.select("ligand","organic")
 cmd.select("chain_a","chain a")
 cmd.select("ligand_a", "ligand and chain_a")
+cmd.select("halligand", "first ligand_a and (name I* or name Br* or name Cl*)")
+cmd.select("bindingsite", "halligand expand 5.0")	# expand selection by 5 angstrom, extend = extend along bonds
 
-cmd.select("halligand", "last ligand and (name I* or name Br* or name Cl*)")
 cmd.select("halcomplex", "first circ_complex and (name I* or name Br* or name Cl*)")
 cmd.select("circ_complex_only","halcomplex extend 5")
 cmd.select("comp_back","circ_complex and not circ_complex_only")
 cmd.select("comp_back_o","comp_back and name o")
 cmd.select("comp_back_c","bound_to comp_back_o")
 cmd.select("comp_back_n","(bound_to comp_back_c) and name N")
+cmd.select("hal", "(e. Cl or e. Br or e. I) and (halligand expand 5)") 
+#find the important bonds for the alignment
+halogen_bonds = find_halogen_bonds()
+print(halogen_bonds)
 
+#select atoms of ligand backbone for alignment
+cmd.select("back_polar","resi "+str(halogen_bonds[0][1])+" and id "+ str(halogen_bonds[0][2]))
+model_back_polar = cmd.get_model("back_polar").atom
+if "S" in model_back_polar[0].name:
+	cmd.select("back_o","resi "+str(halogen_bonds[hal_bond][1])+" and id "+ str(halogen_bonds[hal_bond][2]))
+	cmd.select("back_c","first bound_to back_o")
+	cmd.select("back_n","last bound_to back_o")
+if "O" in model_back_polar[0].name:
+	cmd.select("back_o","resi "+str(halogen_bonds[hal_bond][1])+" and id "+ str(halogen_bonds[hal_bond][2]))
+	cmd.select("back_c","bound_to back_o")
+	cmd.select("back_n","first bound_to back_c")
+
+cmd.select("ligand_circ","br. halligand")
+cmd.select("ligand_back","br. back_o")
+
+#select atoms of halogen structure of complex
 cmd.select("circ_complex","circ_complex_only")
 cmd.select("ciligand", "ligand and name C* and bound_to halligand")
 cmd.select ("cicomplex", "name C* and bound_to halcomplex") 
 
 translation_vector = 0
-pos_halcomplex =[]
-pos_halligand =[]
-pos_cicomplex = []
-pos_ciligand = []
-
-cmd.iterate_state(1, 'halligand', 'pos_halligand.append((x,y,z))')
-cmd.iterate_state(1, 'halcomplex', 'pos_halcomplex.append((x,y,z))')
+pos_halcomplex = get_pos("halcomplex")
+pos_halligand =get_pos("halligand")
+pos_cicomplex = get_pos("cicomplex")
+pos_ciligand = get_pos("ciligand")
 
 
-cmd.iterate_state(1, 'cicomplex', 'pos_cicomplex.append((x,y,z))')
-cmd.iterate_state(1, 'ciligand', 'pos_ciligand.append((x,y,z))')
-
+#get positions of ligand and complex atoms previously selected
 pos_cicomplex = np.array(pos_cicomplex)
 pos_ciligand = np.array(pos_ciligand)
 pos_halcomplex = np.array(pos_halcomplex)
 
-pos_c2complex = []
-pos_c2ligand = []
-pos_c3complex = []
-pos_c3ligand = []
-
-
-
 cmd.select ("c2complex", "first bound_to cicomplex and name c*")
 cmd.select ("c2ligand", "first bound_to ciligand and name c*" )
-
-cmd.iterate_state(1, 'c2ligand', 'pos_c2ligand.append((x,y,z))')
-
-
-print(abs(pos_cicomplex))
 translation_vector1 = -pos_halcomplex
-
 
 #translate molecule from mol file to 0,0,0
 cmd.translate([translation_vector1[0][0],translation_vector1[0][1],translation_vector1[0][2]],"circ_complex",-1,0)
 
 #get coordinates from ligand and mol file complex
-pos_halligand = []
-pos_halcomplex = []
-cmd.iterate_state(1, 'halligand', 'pos_halligand.append((x,y,z))')
-cmd.iterate_state(1, 'halcomplex', 'pos_halcomplex.append((x,y,z))')
+pos_halligand = get_pos("halligand")
+pos_halcomplex = get_pos("halcomplex")
 pos_halligand = np.array(pos_halligand)
 
 #add pseudoatom on x axis to measure angle for rotation around y axis
@@ -293,10 +310,6 @@ cmd.pseudoatom("pseudo_halligand",pos=[pos_halligand[0][0],pos_halligand[0][1],p
 cmd.select("pseudo_ligand","pseudo_*ligand")
 
 #ADD PSEUDO BACKBONE
-
-cmd.select("back_o","first (halligand expand 4.0 and name O and bound_to name C)")
-cmd.select("back_c","bound_to back_o")
-cmd.select("back_n","(bound_to back_c) and name N")
 
 cmd.pseudoatom("pseudo_back_o",pos=[get_pos("back_o")[0][0],get_pos("back_o")[0][1],get_pos("back_o")[0][2]])
 cmd.pseudoatom("pseudo_back_c",pos=[get_pos("back_c")[0][0],get_pos("back_c")[0][1],get_pos("back_c")[0][2]])
@@ -340,53 +353,56 @@ angles_back.append(zero_y("pseudo_back","pseudo_back_n"))
 
 align_ligand("comp_back","back_o",angles_back)
 
-#find_halogen_bonds()
 cmd.zoom("comp_back")
-
 
 selections = cmd.get_names("all")
 
 c=0
 
 #FINALLY ACCESS SEPARATED SELECTIONS TO ALIGN PSEUDOATOMS WITH ENERGIES TO THE BINDING POCKET
-#cmd.select("charges_back",selections_back[0])
 
-#cmd.select("charges_back",selection+" or charges_back")
+#align backbone of ligand to backbone of complex
 align_ligand("charges_backbone","back_o",angles_back)
-	
-#cmd.select("charges_circ",selections_circ[0])
-#for selection in selections_circ:
-#cmd.select("charges_circ",selection+  " or charges_circ")
 cmd.translate([translation_vector1[0][0],translation_vector1[0][1],translation_vector1[0][2]],"charges_circle",-1,0)
 cmd.rotate("z",180,"charges_circle",0,0,origin=[0,0,0])
 cmd.rotate("x",180,"charges_circle",0,0,origin=[0,0,0])
 
+#align halogen structure of ligand to halogen structure of complex
 align_ligand("charges_circle","halligand",angles_circ)
-	
 cmd.select("charged_aas","resn glu+asp+arg+lys+his")
 
-#CLEAN UP SELECTIONS
-cmd.select("ligand_circ","br. halligand")
-cmd.select("ligand_back","br. back_o")
+#clean up selection no longer needed 
+
+
 cmd.show("sticks","ligand_circ")
 cmd.show("sticks","ligand_back")
 cmd.hide("sticks","circ_complex")
 cmd.hide("lines","circ_complex")
-cmd.hide("sticks","ligand")
 cmd.hide("lines","ligand")
 cmd.delete("back_*")
-#cmd.delete("*ligand")
-cmd.delete("*complex")
+cmd.hide("lines")
+cmd.show("lines","bindingsite")
+cmd.delete("hal")
+cmd.delete("polar_atoms")
+cmd.delete("cicomplex")
+cmd.delete("c2complex")
 cmd.delete("comp_back_*")
 cmd.delete("pseud*")
-cmd.delete("*_a")
+cmd.delete("ligand_a")
 cmd.delete("circ_complex_only")
 cmd.delete("halligand")
 cmd.delete("c*ligand")
-cmd.delete("comp_back")
-
-#FIND CHARGED RESIDUES IN CLOUD
-
+cmd.hide("cgo","axes")
+#cmd.delete("comp_back")
+cmd.delete("halcomplex")
+cmd.delete("ligand")
+cmd.hide("sticks","comp_back")
+cmd.hide("lines","comp_back")
+cmd.delete("complex")
+cmd.hide("nonbonded")
+cmd.show("nonbonded","charges_circle")
+cmd.show("nonbonded","charges_backbone")
+#FIND CHARGED RESIDUES IN CHARGE CLOUD
 pos_charges_backbone = get_pos("charges_backbone")
 pos_charges_circle = get_pos("charges_circle")
 
@@ -395,56 +411,49 @@ pos_charges_circle = np.array(pos_charges_circle)
 
 print(pos_charges_backbone[:,0])
 #get start and end positions of cloud to search for charged residues in there
-low_x_1 = min(pos_charges_backbone[:,0])
-low_y_1 = min(pos_charges_backbone[:,1])
-low_z_1 = min(pos_charges_backbone[:,2])
-up_x_1 = max(pos_charges_backbone[:,0])
-up_y_1 = max(pos_charges_backbone[:,1])
-up_z_1 = max(pos_charges_backbone[:,2])
+low_x_1 = min(pos_charges_backbone[:,0]+2)
+low_y_1 = min(pos_charges_backbone[:,1]+2)
+low_z_1 = min(pos_charges_backbone[:,2]+2)
+up_x_1 = max(pos_charges_backbone[:,0]-2)
+up_y_1 = max(pos_charges_backbone[:,1]-2)
+up_z_1 = max(pos_charges_backbone[:,2]-2)
 
-low_x_2 = min(pos_charges_circle[:,0])
-low_y_2 = min(pos_charges_circle[:,1])
-low_z_2 = min(pos_charges_circle[:,2])
-up_x_2 = max(pos_charges_circle[:,0])
-up_y_2 = max(pos_charges_circle[:,1])
-up_z_2 = max(pos_charges_circle[:,2])
-
-print(low_x_1,up_x_1)
-print(low_y_1,up_y_1)
-print(low_z_1,up_z_1)
-
-print(low_x_2,up_x_2)
-print(low_y_2,up_y_2)
-print(low_z_2,up_z_2)
+low_x_2 = min(pos_charges_circle[:,0]+2)
+low_y_2 = min(pos_charges_circle[:,1]+2)
+low_z_2 = min(pos_charges_circle[:,2]+2)
+up_x_2 = max(pos_charges_circle[:,0]-2)
+up_y_2 = max(pos_charges_circle[:,1]-2)
+up_z_2 = max(pos_charges_circle[:,2]-2)
 
 
-cmd.pseudoatom("a",pos=[pos_charges_backbone[5][0],pos_charges_backbone[5][1],pos_charges_backbone[5][2]])
-cmd.pseudoatom("ba",pos=[low_x_2,low_y_2,low_z_2])
 cmd.select("charged_aas","resn glu+asp+arg+lys+his")
-
+cmd.delete("chain_a")
 aas = []
 
 
 for atom in cmd.get_model("charged_aas").atom:
-	if ((atom.coord[0] < up_x_2 and atom.coord[0] > low_x_2) and \
-	(atom.coord[1] < up_y_2 and atom.coord[1] > low_y_2) and \
-	(atom.coord[2] < up_z_2 and atom.coord[2] > low_z_2)):
-		#print(atom.coord[0],atom.coord[1],atom.coord[2])
-		aas.append((atom.resn,atom.resi))
+	if atom.name == "OD1" or atom.name == "OD2" or (atom.resn == "HIS" and atom.name =="HB2") or \
+	atom.name == "HZ1" or atom.name == "HH12" or atom.name == "NZ" or \
+	atom.name == "NE2" or (atom.name == "N" and atom.resn == "HIS") or atom.name =="OE1" or \
+	atom.name == "OE2" or atom.name == "NH2" or atom.name == "NH1":
+		if ((atom.coord[0] < up_x_2 and atom.coord[0] > low_x_2) and \
+		(atom.coord[1] < up_y_2 and atom.coord[1] > low_y_2) and \
+		(atom.coord[2] < up_z_2 and atom.coord[2] > low_z_2)):
+			aas.append((atom.resn,atom.resi))
 
 for atom in cmd.get_model("charged_aas").atom:
-	if ((atom.coord[0] < up_x_1 and atom.coord[0] > low_x_1) and \
-	(atom.coord[1] < up_y_1 and atom.coord[1] > low_y_1)  and \
-	(atom.coord[2] < up_z_1 and atom.coord[2] > low_z_1)):
-		#print(atom.coord[0],atom.coord[1],atom.coord[2])
-		aas.append((atom.resn,atom.resi))
+	if atom.name == "OD1" or atom.name == "OD2" or (atom.resn == "HIS" and atom.name =="HB2") or \
+	atom.name == "HZ1" or atom.name == "HH12" or atom.name == "NZ" or \
+	atom.name == "NE2" or (atom.name == "N" and atom.resn == "HIS") or atom.name =="OE1" or \
+	atom.name == "OE2" or atom.name == "NH2" or atom.name == "NH1":
+		if ((atom.coord[0] < up_x_1 and atom.coord[0] > low_x_1) and \
+		(atom.coord[1] < up_y_1 and atom.coord[1] > low_y_1)  and \
+		(atom.coord[2] < up_z_1 and atom.coord[2] > low_z_1)):
+			aas.append((atom.resn,atom.resi))
 aas = set(aas)
 for res in aas:
-	cmd.select(str(res[0])+"_"+res[1],"resn "+res[0]+" and resi "+res[1])
+	cmd.select(str(res[0])+"_"+res[1],"chain a and resn "+res[0]+" and resi "+res[1])
 
-
-for atom in cmd.get_model("charges_backbone").atom:
-	cmd.select("a","charges_backbone and id "+str(atom.id)+" expand 1.0 and charged_aas")
 
 
 """
